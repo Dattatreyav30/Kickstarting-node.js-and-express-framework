9776b677ddf17494d697ecf3113b26aef1bd55a0
dattatreyav30 <dattatreya361@gmail.com>
2023-03-09 14:44:03 +0530

    hello world

---
+++ firstApp.js
@@ -0,0 +1,4 @@
+// const fs = require('fs');  //import some file systme functionalities so that we can use them
+// fs.writeFileSync('hello.txt', "Hello from node.js"); //create newfile
+// //why node js - to write server side code
+console.log('hello world');
