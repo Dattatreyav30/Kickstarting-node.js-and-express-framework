d0655493fb3324e209e7ba375a0f35fbf25cb704
dattatreyav30 <dattatreya361@gmail.com>
2023-03-10 14:11:58 +0530

    req/res

--- server.js
+++ server.js
@@ -17,7 +17,29 @@
 // server.listen(3000); // listen the incoming requests
 
 const http = require('http');
-const server  = http.createServer((req,res) =>{
-    console.log('dattatreya')
+const server = http.createServer((req, res) => {
+    //console.log(req.url,req.method,req.headers);
+    //process.exit();  to exit from the event loop
+    if (req.url === '/home') {
+        res.setHeader('content-type', 'text/html')
+        res.write('<html>');
+        res.write('<head><title>this is my first server</title></head>');
+        res.write('<body><h1>welcome home</h1></body>')
+        res.end();
+    }
+    else if (req.url === '/about') {
+        res.setHeader('content-type', 'text/html')
+        res.write('<html>');
+        res.write('<head><title>this is my first server</title></head>');
+        res.write('<body><h1>welcome to about us page</h1></body>')
+        res.end();
+    }
+    else if (req.url === '/node') {
+        res.setHeader('content-type', 'text/html')
+        res.write('<html>');
+        res.write('<head><title></title></head>');
+        res.write('<body><h1>welcome to my node js project</h1></body>')
+        res.end();
+    }
 })
-server.listen(4000);
+server.listen(5000);
