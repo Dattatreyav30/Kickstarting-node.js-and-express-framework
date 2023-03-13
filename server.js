034d3c4ea326ac6a0b84353c578db8d5ce58024c
dattatreyav30 <dattatreya361@gmail.com>
2023-03-13 10:50:40 +0530

    C:/Program Files/Git/addproduct , post requests

--- server.js
+++ server.js
@@ -52,16 +52,24 @@ const express = require('express');
 
 const app = express();//creating expresss app 
 
-app.use((req, res, next) => {
-    console.log('in the middleware');
-    next();
-});  //use helps to add middleware functions ,function will be executed for every incoming requests
+//use helps to add middleware functions ,function will be executed for every incoming requests
 //next is another function ,will execute when we want to handle next middleware
 
-app.use((req, res, next) => {
-    res.send('{ key1: value }')
-    console.log('in the another middleware');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  //res.send('<h1>hello from express</h1>');
+  res.send('<form action="/product" method = "POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">add product</button></form>');
+});
+
+app.post('/product',(req,res,next)=>{
+  console.log(req.body);
+  res.redirect('/');
+})
+app.use('/', (req, res, next) => {
+  //res.send('<h1>hello from express</h1>');
+  res.send('<h1>hi from express!</h1>');
 });
 //const server = http.createServer(app);
-
 app.listen(2000);
